b06e4aa5ec81aaa0f76710c25e396d8df6802dc0
mrvan-design <taitomgamer2409@gmail.com>
2025-11-17 20:58:13 +0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,8 +2,11 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
+
+  # Bỏ qua các check không cần thiết
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
   endpoints {
     ec2 = "http://localhost:4566"
